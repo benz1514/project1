f2bf21d8a0cc119c4404a24e701abbf8b6485c43
benz1514 <78993088+benz1514@users.noreply.github.com>
2021-02-13 14:28:17 +0700

    เพิ่มlayout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">หน้าแรก</a> |
-<a href="/about" >about</a> |
-    <a href="/contact" >contact</a>
-<h1>หน้า contact</h1>
+@extends('layout.master')
 
-</body>
-</html>
+@section('content')
+
+<h1>หน้า contact</h1>
+@endsection
